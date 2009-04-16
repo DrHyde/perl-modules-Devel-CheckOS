b67d3401b4e6b3d70f7b095ad704a91e0d969796
David Cantrell <david@cantrell.org.uk>
2009-04-16 01:12:49 +0100

    Argh, 1.55

--- lib/Devel/CheckOS.pm
+++ lib/Devel/CheckOS.pm
@@ -7,7 +7,7 @@ use Exporter;
 
 use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
 
-$VERSION = '1.54';
+$VERSION = '1.55';
 
 # localising prevents the warningness leaking out of this module
 local $^W = 1;    # use warnings is a 5.6-ism
