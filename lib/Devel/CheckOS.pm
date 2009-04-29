58e46bfad8e6928a17fec8572cbb0d6f67469b3d
David Cantrell <david@cantrell.org.uk>
2009-04-29 15:52:18 +0100

    Change Devel::CheckOS doco to point at git repo

--- lib/Devel/CheckOS.pm
+++ lib/Devel/CheckOS.pm
@@ -308,13 +308,13 @@ Thanks to Matt Kraai for information about QNX.
 Thanks to Kenichi Ishigaki and Gabor Szabo for reporting a bug on Windows,
 and to the former for providing a patch.
 
-=head1 CVS
+=head1 SOURCE CODE REPOSITORY
 
-L<http://drhyde.cvs.sourceforge.net/drhyde/perlmodules/Devel-CheckOS/>
+L<http://www.cantrell.org.uk/cgit/cgit.cgi/perlmodules/>
 
 =head1 COPYRIGHT and LICENCE
 
-Copyright 2007 David Cantrell
+Copyright 2007-2009 David Cantrell
 
 This software is free-as-in-speech software, and may be used, distributed, and modified under the terms of either the GNU General Public Licence version 2 or the Artistic Licence. It's up to you which one you use. The full text of the licences can be found in the files GPL2.txt and ARTISTIC.txt, respectively.
 
