cecb92a4169d3c1f40136ec1473662503efaaec7
David Cantrell <david@cantrell.org.uk>
2016-10-30 17:02:26 +0000

    revert 5d0263a and leave a comment explaining why

--- lib/Devel/CheckOS.pm
+++ lib/Devel/CheckOS.pm
@@ -4,9 +4,6 @@ use strict;
 use warnings;
 use Exporter;
 
-use File::Find::Rule;
-use File::Spec;
-
 use vars qw(@ISA @EXPORT_OK %EXPORT_TAGS);
 
 our $VERSION = '1.77';
@@ -169,6 +166,15 @@ should Just Work anyway.
 my ($re_Devel, $re_AssertOS);
 
 sub list_platforms {
+    # need to lazily load these cos the module gets use()d in Makefile.PL,
+    # at which point pre-reqs might not be installed. This function isn't
+    # used in Makefile.PL so we can live without 'em.
+    eval " # only load these if needed
+        use File::Find::Rule;
+        use File::Spec;
+    ";
+    
+    die($@) if($@);
     if (!$re_Devel) {
         my $case_flag = File::Spec->case_tolerant ? '(?i)' : '';
         $re_Devel    = qr/$case_flag ^Devel$/x;
