99bf675edd5486016a4054d5f201d0530ae7af08
David Cantrell <david@cantrell.org.uk>
2020-02-21 19:55:15 +0000

    Add modern MacOSes; note the OS's change of name (but don't change the module names)

--- lib/Devel/AssertOS/MacOSX/v10_15.pm
+++ lib/Devel/AssertOS/MacOSX/v10_15.pm
@@ -1,24 +1,28 @@
-package Devel::AssertOS::MacOSX;
+package Devel::AssertOS::MacOSX::v10_15;
 
 use Devel::CheckOS;
 use strict;
 use warnings;
 no warnings 'redefine';
 
-our $VERSION = '1.3';
+our $VERSION = '1.0';
 
-sub os_is { $^O =~ /^darwin$/i ? 1 : 0; }
+sub os_is {
+    Devel::CheckOS::os_is('MacOSX') &&
+    `sw_vers -productVersion` =~ /^10\.15\./ ? 1 : 0;
+}
+
+sub expn { "The operating system is some version of OS X Catalina (10.15)" }
 
 Devel::CheckOS::die_unsupported() unless(os_is());
 
-sub expn { "Either Mac OS X, or OS X, or MacOS, they're all the same thing really" }
-
 =head1 COPYRIGHT and LICENCE
 
-Copyright 2007 - 2014 David Cantrell
+Copyright 2020 David Cantrell
 
 This software is free-as-in-speech software, and may be used, distributed, and modified under the terms of either the GNU General Public Licence version 2 or the Artistic Licence. It's up to you which one you use. The full text of the licences can be found in the files GPL2.txt and ARTISTIC.txt, respectively.
 
 =cut
 
 1;
+
