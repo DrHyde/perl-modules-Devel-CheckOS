d11d2c745390ba48c1a0f656345f24be5f684a1f
David Cantrell <david@cantrell.org.uk>
2024-05-10 17:51:45 +0100

    Make the Makefile.PL driver script more robust

--- makefile-expect-driver.pl
+++ makefile-expect-driver.pl
@@ -2,11 +2,12 @@ use strict;
 use warnings;
 
 use Expect;
+use File::Spec;
 
-(my @platforms = @ARGV) ||
+(my @platforms_wanted = @ARGV) ||
     die("You need to tell me what platforms to expect\n");
-my $platforms_regex = join('|', @platforms);
-my $platforms_found = 0;
+my $platforms_regex = '^Are you using ('.join('|', @platforms_wanted).')\\?';
+my @platforms_found;
 my $e = Expect->new();
 
 $ENV{AUTOMATED_TESTING} = 1;
@@ -14,16 +15,18 @@ $e->spawn($^X, 'Makefile.PL');
 
 my($eof, $timeout) = (0, 0);
 while(!$eof && !$timeout) {
-    my $pattern_index = $e->expect(2,
+    my $pattern_index = $e->expect(
+        50,
         [ eof     => sub { $eof++;     } ],
         [ timeout => sub { $timeout++; } ],
         -re => qr/(Are you using (.*)\?)/,
-        -re => qr/./
     );
     if($pattern_index && $pattern_index == 3) {
         my $matched = $e->match();
-        $platforms_found++;
-        if($matched =~ /^Are you using ($platforms_regex)/) {
+        (my $os_detected = $matched) =~ s/^Are you using (.*?)\?.*/$1/;
+        push @platforms_found, $os_detected;
+
+        if($matched =~ /$platforms_regex/) {
             $e->send("\n");
         } else {
             $e->send("n\n");
@@ -31,5 +34,24 @@ while(!$eof && !$timeout) {
     }
 }
 
-die("Didn't match all expected platforms: expected [".join(', ', @platforms)."]\n")
-    if($platforms_found != @platforms);
+my @errors = ();
+foreach my $wanted_platform (@platforms_wanted) {
+    if(!grep { $_ eq $wanted_platform } @platforms_found) {
+        push @errors, "Wanted $wanted_platform, but not detected";
+        (my $filename_os = $wanted_platform) =~ s/::/-/g;
+        my $test_file = File::Spec->catfile('t', "XX-no-autodetect-$^O-as-$filename_os.t");
+        open(my $fh, '>', $test_file) || die("Couldn't write $test_file: $!\n");
+        print $fh 'print "1..1\\nnot ok 1\\n"';
+        close($fh);
+    }
+}
+foreach my $got_platform (@platforms_found) {
+    if(!grep { $_ eq $got_platform } @platforms_wanted) {
+        push @errors, "Got $got_platform, unexpectedly";
+    }
+}
+die(
+    "Didn't match all (and only!) expected platforms:\n".
+    join("\n", map { "  $_" } @errors).
+    "\n"
+) if(@errors);
